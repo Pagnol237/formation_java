e554f38cb628a816e30e982197bfa13bbf22f893
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-07-23 21:09:52 -0500

    clonne
    ajous de la structure de base dans le clone

---
+++ src/main/java/com/pagnol/formation/Main.java
@@ -0,0 +1,9 @@
+package com.pagnol.formation;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("le debut de la formation et je promet etre le  meilleur");
+    }
+}
